3bdda7f8c1432ba0168923ed500372d2e8898dec
黄钊 <h13202021470@gmail.com>
2019-07-15 22:41:00 +0800

    提交，详情看README

---
+++ public/m/js/cart.js
@@ -0,0 +1,86 @@
+/**
+ * 检测用户是否登录
+ * 
+ */
+$.ajax({
+	url: '/user/queryUserMessage',
+	type: 'get',
+	// 将异步改为同步
+	async: false,
+	success: function(res) {
+		if(res.error && res.error === 400) {
+			location.href = 'login.html'
+		}
+	}
+})
+// 页码数
+let page = 1
+// 用来修改this 指向
+let that = null
+// 存储页面数据
+let html = ''
+
+$(function() {
+	// 初始化上拉加载更多--同时第一次获取购物车数据
+	initPullRefresh()
+	// 编辑商品
+	editGoods()
+})
+
+
+/**
+ * 初始化上拉加载更多
+ * 
+ */
+function initPullRefresh() {
+	mui.init({
+	  pullRefresh : {
+	    container:'#goodsContent',//待刷新区域标识，querySelector能定位的css选择器均可，比如：id、.class等
+	    up : {
+	      height:100,//可选.默认50.触发上拉加载拖动距离
+	      auto:true,//可选,默认false.自动上拉加载一次
+	      contentrefresh : "正在加载...",//可选，正在加载状态时，上拉加载控件上显示的标题内容
+	      contentnomore:'没有更多数据了',//可选，请求完毕若没有更多数据时显示的提醒内容；
+	      callback :getData //必选，刷新函数，根据具体业务来编写，比如通过ajax从服务器获取新数据；
+	    }
+	  }
+	})
+}
+
+// 初始化上拉加载更多是，获取购物车数据
+function getData() {
+	if(!that) {
+		that = this
+	}
+	$.ajax({
+		url: '/cart/queryCartPaging',
+		type: 'get',
+		data: {
+			page: page++,
+			pageSize: 7
+		},
+		success: function(res) {
+			if(res.data && res.data.length > 0) {
+				html += template('artTempl', {
+					data: res.data
+				})
+				$('#goodsList').html(html)
+				that.endPullupToRefresh(false)
+			} else {
+				that.endPullupToRefresh(true)
+			}
+		}
+	})
+}
+
+// 编辑商品
+function editGoods() {
+	$('#goodsList').on('tap', '.eidt-btn', function() {
+		let allSize = this.getAttribute('data-allSize')
+		let id = this.getAttribute('data-id')
+    let html = template('artTempl2', {
+    	allSize: allSize
+    })
+		mui.confirm(html, '请选择')
+	})
+}
--- public/m/js/detail.js
+++ public/m/js/detail.js
@@ -1,5 +1,19 @@
 $(function() {
-	let id = getParamsByUrl(location.href, 'id')
+	getGoodsInfo()
+
+  selectSize()
+	
+	addCart()
+})
+// 商品尺码
+let size = 0
+// 商品ID
+let id = 0
+/**
+ * 获取商品信息
+ */
+function getGoodsInfo() {
+  id = getParamsByUrl(location.href, 'id')
 	$.ajax({
 		url: '/product/queryProductDetail',
 		type: 'get',
@@ -7,11 +21,79 @@ $(function() {
 			id: id
 		},
 		success: function(res) {
-		 let html =	template('detailTemp',{
-				data: res
-			})
-		 $('#contentWrapper').html(html)
+		  let html =	template('detailTemp', res)
+		  $('#contentWrapper').html(html)
 		 mui('.mui-numbox').numbox()
+		  //获得slider插件对象 -- 因为动态生成的轮播图，所以要重新初始化
+			var gallery = mui('.mui-slider');
+			gallery.slider();
 		}
 	})
-})
+}
+
+/**
+ * 尺码选择
+ */
+function selectSize() {
+	// 事件委托
+	$('#contentWrapper').on('tap', '.size-wrapper .size-item', function() {
+		$(this).addClass('active').siblings('span').removeClass('active')
+		size = $(this).text()
+	})
+
+}
+
+/**
+ * 加入购物车 -- 需判断用户是否登录了
+ * 1. 获取加入购物车按钮，并添加点击事件
+ * 2. 判断用户是否选择了尺码
+ * 3. 调用加入购物车接口
+ * 4. 提示用户加入购物车成功,是否跳转到购物车页面
+ */
+
+function addCart() {
+	$('#addCart').on('tap', function() {
+	 /**
+	 * 检测用户是否登录
+	 * 
+	 */
+		$.ajax({
+			url: '/user/queryUserMessage',
+			type: 'get',
+			// 将异步改为同步
+			async: false,
+			success: function(res) {
+				if(res.error && res.error === 400) {
+					location.href = 'login.html'
+				} 
+			}
+		})
+
+		if(!size) {
+			mui.alert('请选择尺码')
+			return
+		}
+     
+    let num = mui('.mui-numbox').numbox().getValue()
+		$.ajax({
+			url: ' /cart/addCart',
+			type: 'post',
+			data: {
+				productId: id,
+				num: num,
+				size: size
+			},
+			success: function(res) {
+				if(res.success) {
+					mui.confirm('添加成功，去购车看看', function(res) {
+						if(res.index === 1) {
+							location.href = 'cart.html'
+						}
+					})
+				} else {
+					mui.alert('网络有误，请稍后。。。。')
+				}
+			}
+		})
+	})
+}
